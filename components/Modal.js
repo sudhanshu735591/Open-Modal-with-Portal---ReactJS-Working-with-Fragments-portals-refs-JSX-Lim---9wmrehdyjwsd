ee8877a9c7cd92c8552c41e4a4b968e93c929719
Sudhanshu <ersshekhar05@gmail.com>
2023-12-17 17:49:21 +0530

    code

--- components/Modal.js
+++ components/Modal.js
@@ -2,12 +2,12 @@
 import HomePage from '@/pages';
 import ReactDOM, { createPortal } from 'react-dom';
 
-const Modal = ({ children, isOpen }) => {
+const Modal = ({ children, isOpen, onClose }) => {
   if (!isOpen) return null;
 
     //add code here
     return ReactDOM.createPortal(
-      <div className='modal'>
+      <div className='modal' onClick={onClose}>
         <div style={{ backgroundColor: 'white', padding: '2rem', position: 'relative' }} onClick={(e) => e.stopPropagation()} >
           {children}
         </div>
